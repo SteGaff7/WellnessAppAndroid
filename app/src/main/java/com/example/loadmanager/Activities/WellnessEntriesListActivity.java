78647f8a88d4016ca901cae269a4a5c4a784aa62
Stephen Gaffney <stephen.gaffney@ucdconnect.ie>
2020-04-10 14:22:14 +0100

    Added buttons to wellness entries that change the score that is displayed.

--- app/src/main/java/com/example/loadmanager/Activities/WellnessEntriesListActivity.java
+++ app/src/main/java/com/example/loadmanager/Activities/WellnessEntriesListActivity.java
@@ -1,10 +1,12 @@
 package com.example.loadmanager.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,10 +62,23 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView wellnessEntryErrorTextView;
     LineChart chart;
+    LinearLayout graphButtonBar;
+    Button sleepGraphButton;
+    Button energyGraphButton;
+    Button sorenessGraphButton;
+    Button moodGraphButton;
+    Button stressGraphButton;
+    Button totalGraphButton;
+
     private MyAdapter mAdapter;
     private ArrayList<JSONObject> mAdapterData = new ArrayList<>();
-    private ArrayList<String> graphDateList = new ArrayList<>();
-    private ArrayList<Float> graphTotalScoreList = new ArrayList<>();
+    private List<String> graphDateList = new ArrayList<>();
+    private List<Float> graphSleepScoreList = new ArrayList<>();
+    private List<Float> graphEnergyScoreList = new ArrayList<>();
+    private List<Float> graphSorenessScoreList = new ArrayList<>();
+    private List<Float> graphMoodScoreList = new ArrayList<>();
+    private List<Float> graphStressScoreList = new ArrayList<>();
+    private List<Float> graphTotalScoreList = new ArrayList<>();
 
 
     @Override
@@ -69,17 +86,64 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellness_entries_list);
 
-//        Initialize adapter with no data set
+        chart = findViewById(R.id.lineChart);
+        graphButtonBar = findViewById(R.id.graphButtonBar);
+
+        sleepGraphButton = findViewById(R.id.sleepGraphButton);
+        energyGraphButton = findViewById(R.id.energyGraphButton);
+        sorenessGraphButton = findViewById(R.id.sorenessGraphButton);
+        moodGraphButton = findViewById(R.id.moodGraphButton);
+        stressGraphButton = findViewById(R.id.stressGraphButton);
+        totalGraphButton = findViewById(R.id.totalGraphButton);
+
+//      Initialize adapter with no data set
         mAdapter = new MyAdapter(getApplicationContext(), mAdapterData);
 
         recyclerView = findViewById(R.id.myRecycleView);
-//        Set empty adapter
+//      Set empty adapter
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         wellnessEntryErrorTextView = findViewById(R.id.wellnessEntryErrorTextView);
 
         getWellnessEntriesVolleyTask();
+
+        sleepGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Sleep");
+            }
+        });
+        energyGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Energy");
+            }
+        });
+        sorenessGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Soreness");
+            }
+        });
+        moodGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Mood");
+            }
+        });
+        stressGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Stress");
+            }
+        });
+        totalGraphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateChart(7, false, "Total");
+            }
+        });
     }
 
     private void getWellnessEntriesVolleyTask() {
@@ -105,9 +169,8 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-                        Toast.makeText(WellnessEntriesListActivity.this, "This is a response message", Toast.LENGTH_SHORT).show();
-//                        Notify on main thread
-                        mAdapter.notifyDataSetChanged();;
+//                        Toast.makeText(WellnessEntriesListActivity.this, "This is a response message", Toast.LENGTH_SHORT).show();
+                        ;
                     }
                 }
                 ,
@@ -145,24 +208,13 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
                     JSONArray jsonArray = new JSONArray(jsonString);
 
 //                  Parse on background thread
-//                    If doesn't return true do something..?
                     parseResponse(jsonArray);
-
-//                    Start a thread on main thread (Not needed)
-
-//                    WellnessEntriesListActivity.this.runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            mAdapter.notifyDataSetChanged();
-//                        }
-//                    });
 
                     return Response.success(jsonArray, HttpHeaderParser.parseCacheHeaders(response));
                 } catch (UnsupportedEncodingException | JSONException e) {
                     return Response.error(new ParseError(e));
                 }
             }
-
         };
         requestQueue.add(request);
     }
@@ -171,7 +223,7 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
      * Function that parses volley response object, updates adapter and graph data and generates graph
      * @param response
      */
-    private boolean parseResponse(JSONArray response) throws JSONException {
+    private void parseResponse(JSONArray response) throws JSONException {
 
 //      Clear adapter data set
         mAdapterData.clear();
@@ -199,30 +251,76 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
 //          Add to chart data
             String dateStr = (String) wellnessEntry.get("date");
 
+            float sleepScore = (float) wellnessEntry.getInt("sleep_score");
+            float energyScore = (float) wellnessEntry.getInt("energy_score");
+            float sorenessScore = (float) wellnessEntry.getInt("soreness_score");
+            float moodScore = (float) wellnessEntry.getInt("mood_score");
+            float stressScore = (float) wellnessEntry.getInt("stress_score");
             float totalScore = (float) wellnessEntry.getInt("total_score");
 
             graphDateList.add(dateStr);
+            graphSleepScoreList.add(sleepScore);
+            graphEnergyScoreList.add(energyScore);
+            graphSorenessScoreList.add(sorenessScore);
+            graphMoodScoreList.add(moodScore);
+            graphStressScoreList.add(stressScore);
             graphTotalScoreList.add(totalScore);
         }
 
+//      Notify adapter on main thread
+        WellnessEntriesListActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
         int number = 7;
-        populateChart(number, false);
-        return true;
+        populateChart(number, false, "Total");
     }
 
-    private void populateChart(int number, boolean days) {
+    private void populateChart(int number, boolean days, final String scoreType) {
 
         List<Entry> dataEntriesList = new ArrayList<>();
         List<String> dataDateList = new ArrayList<>();
 
-        chart = findViewById(R.id.lineChart);
+        final Button activeButton;
+        List<Float> activeDataList;
+
+        switch(scoreType)
+        {
+            case "Sleep":
+                activeButton = sleepGraphButton;
+                activeDataList = graphSleepScoreList;
+                break;
+            case "Energy":
+                activeButton = energyGraphButton;
+                activeDataList = graphEnergyScoreList;
+                break;
+            case "Soreness":
+                activeButton = sorenessGraphButton;
+                activeDataList = graphSorenessScoreList;
+                break;
+            case "Mood":
+                activeButton = moodGraphButton;
+                activeDataList = graphMoodScoreList;
+                break;
+            case "Stress":
+                activeButton = stressGraphButton;
+                activeDataList = graphStressScoreList;
+                break;
+            default:
+                activeButton = totalGraphButton;
+                activeDataList = graphTotalScoreList;
+                break;
+        }
 
         if (days) {
 //          Use different function to get data lists
             ;
         } else {
 //          Call the method that will parse list appropriately for entries
-            List[] result = makeGraphData(number);
+            List[] result = makeGraphData(number, activeDataList);
             dataEntriesList = result[0];
             dataDateList = result[1];
         }
@@ -247,19 +345,46 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
             }
             xAxis.setValueFormatter(xAxisFormatter);
 
-            String label = new String("Total Score");
+            String label = (scoreType + " Score");
             LineDataSet dataSet = new LineDataSet(dataEntriesList, label);
 //          dataSet.setColor();
 
             LineData lineData = new LineData(dataSet);
             chart.setData(lineData);
             chart.setDescription(null);
+
+//          Refresh chart
+            WellnessEntriesListActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    chart.invalidate();
+//                  Show button bar & disable total button as it is default
+                    graphButtonBar.setVisibility(View.VISIBLE);
+                    activeButton.setEnabled(false);
+
+//                  TO DO: Move out
+                    Resources resources = getApplicationContext().getResources();
+                    int black = ResourcesCompat.getColor(resources, R.color.black, null);
+                    int red = ResourcesCompat.getColor(resources, R.color.red, null);
+
+                    activeButton.setTextColor(black);
+
+//                  Reset non active buttons
+                    for(int i =0; i< graphButtonBar.getChildCount(); i++){
+                        View v = graphButtonBar.getChildAt(i);
+                        if(v instanceof Button && v != activeButton){
+                            v.setEnabled(true);
+                            ((Button) v).setTextColor(red);
+                        }
+                    }
+                }
+            });
         } else {
             chart.setNoDataText("Not enough entries to show graph");
-
+//          Refresh chart
+            chart.invalidate();
         }
-//      Refresh chart
-        chart.invalidate();
+
     }
 
 //  Value Formatter class
@@ -279,16 +404,16 @@ public class WellnessEntriesListActivity extends AppCompatActivity {
         }
     }
 
-    private List[] makeGraphData(int numEntries) {
+    private List[] makeGraphData(int numEntries, List<Float> dataList) {
         List[] result = new ArrayList[2];
 
-        if (numEntries > graphTotalScoreList.size()) {
+        if (numEntries > dataList.size()) {
 //          Or make the entries before null like it will be in the days format
-            numEntries = graphTotalScoreList.size();
+            numEntries = dataList.size();
         }
 
         List<String> dates = new ArrayList<>(graphDateList.subList(0, numEntries));
-        List<Float> scores = new ArrayList<>(graphTotalScoreList.subList(0, numEntries));
+        List<Float> scores = new ArrayList<>(dataList.subList(0, numEntries));
         List<Entry> entries = new ArrayList<>();
 
 //      Reverse, chronologically ordered for graph
